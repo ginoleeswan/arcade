1df5d76035a53f8fcc1e3ab9025a708d465ef034
Gino Swanepoel <ginoleemusic@live.com>
2021-09-13 19:39:23 +0200

    implemented shared elements

--- app/navigation/HomeNavigation.js
+++ app/navigation/HomeNavigation.js
@@ -2,18 +2,38 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavBar from "./NavBar";
 import GameInfoScreen from "../screens/GameInfoScreen";
+import { enableScreens } from "react-native-screens";
+import HomeScreen from "../screens/HomeScreen";
+import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 
-const Stack = createNativeStackNavigator();
+enableScreens();
+
+const Stack = createSharedElementStackNavigator();
 
 export const HomeNavigation = () => {
   return (
     <Stack.Navigator
+      mode="modal"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Home" component={NavBar} />
-      <Stack.Screen name="Game Info" component={GameInfoScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+
+      <Stack.Screen
+        name="Game Info"
+        component={GameInfoScreen}
+        sharedElements={(route) => {
+          return [
+            {
+              id: route.params.game.id,
+              animation: "fade",
+              resize: "clip",
+              // align: ''left-top'
+            },
+          ];
+        }}
+      />
     </Stack.Navigator>
   );
 };
