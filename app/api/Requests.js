0e6ef392f34dd6a29f1afde0d918246c3f3e4a2c
Gino Swanepoel <ginoleemusic@live.com>
2021-09-19 01:10:49 +0200

    added more chips

--- app/api/Requests.js
+++ app/api/Requests.js
@@ -5,7 +5,13 @@ const requests = {
   fetchMustPlayGames: `collections/must-play/feed/?key=${API_KEY}`,
   fetchIndieGames: `games/?key=${API_KEY}&genres=indie`,
   fetchRacingGames: `games/?key=${API_KEY}&genres=racing`,
+  fetchAdventureGames: `games/?key=${API_KEY}&genres=adventure`,
   fetchStrategyGames: `games/?key=${API_KEY}&genres=strategy`,
+  fetchRPGGames: `games/?key=${API_KEY}&genres=role-playing-games-rpg`,
+  fetchSimulationGames: `games/?key=${API_KEY}&genres=simulation`,
+  fetchSportGames: `games/?key=${API_KEY}&genres=sports`,
+  fetchCasualGames: `games/?key=${API_KEY}&genres=casual`,
+  fetchShooterGames: `games/?key=${API_KEY}&genres=shooter`,
   getGameByID: `games/`,
 };
 
