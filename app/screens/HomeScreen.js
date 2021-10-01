c6c3385d6b6765fe363fed2d86f9310022714b86
Gino Swanepoel <ginoleemusic@live.com>
2021-10-01 22:20:50 +0200

    redesigned the home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -5,22 +5,37 @@ import {
   Text,
   ImageBackground,
   FlatList,
+  Animated,
+  StatusBar,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { COLORS } from "../styles/colors";
 import axios from "../api/axios";
 import requests from "../api/Requests";
 import { GamesContext } from "../context/GamesContext";
 import { LinearGradient } from "expo-linear-gradient";
 import { GameCard } from "../components/GameCard";
+import { GameInfoCard } from "../components/GameInfoCard";
+import { GameCardWide } from "../components/GameCardWide";
 import { Chip, TabView, Icon } from "react-native-elements";
+import { SearchBar } from "react-native-elements";
 
 import { Tab } from "react-native-elements";
 import { Modal } from "react-native";
 import { ActivityIndicator } from "react-native";
+import Carousel from "react-native-snap-carousel";
+import { Dimensions } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Keyboard } from "react-native";
+import { GameLoadingContext } from "../context/GameLoadingContext";
 
 const HomeScreen = ({ navigation }) => {
   const [games, setGames] = useContext(GamesContext);
+  const [gameLoading, setGameLoading] = useContext(GameLoadingContext);
+  const [query, setQuery] = useState("");
   const [selectedChip, setSelectedChip] = useState(0);
   const [loading, setLoading] = useState(false);
   const [indexToAnimate, setIndexToAnimate] = useState(0);
@@ -130,20 +145,51 @@ const HomeScreen = ({ navigation }) => {
     },
   ]);
 
-  const fetchGames = async (type, chipID) => {
+  const [gameSectionTitle, setGameSectionTitle] = useState(
+    chips[selectedChip].title
+  );
+
+  const [gameSectionIconName, setGameSectionIconName] = useState(
+    chips[selectedChip].iconName
+  );
+
+  const [gameSectionIconType, setGameSectionIconType] = useState(
+    chips[selectedChip].iconType
+  );
+
+  const insets = useSafeAreaInsets();
+
+  const scrollY = new Animated.Value(0);
+  const translateY = scrollY.interpolate({
+    inputRange: [0, 200 + insets.top],
+    outputRange: [0, insets.top - 260],
+    extrapolate: "clamp",
+  });
+  const fadeOutOnScroll = scrollY.interpolate({
+    inputRange: [0, 120],
+    outputRange: [1, 0],
+    extrapolate: "clamp",
+  });
+
+  const fetchGames = async (chip) => {
     setGames(null);
-    setSelectedChip(chipID);
+    setSelectedChip(chip.id);
+    setGameSectionTitle(chip.title);
+    setGameSectionIconName(chip.iconName);
+    setGameSectionIconType(chip.iconType);
     try {
       setLoading(true);
-      const request = await axios.get(type);
+      const request = await axios.get(chip.search);
 
-      if (type == fetchMustPlayGames) {
+      if (chip.search == fetchMustPlayGames) {
         const newGameList = request.data.results;
         let items = newGameList.map((item) => {
           return {
             result: item.game,
             id: item.game.id,
             name: item.game.name,
+            parent_platforms: item.game.parent_platforms,
+            rating: item.game.rating,
             background_image: item.game.background_image,
           };
         });
@@ -160,8 +206,44 @@ const HomeScreen = ({ navigation }) => {
     }
   };
 
+  const handleSearch = async (text) => {
+    setQuery("");
+    setGames(null);
+    setGameSectionTitle("Search");
+    setGameSectionIconName("search");
+    setGameSectionIconType("material-icons");
+    // const formattedQuery = text.toLowerCase();
+    // const data = heroNames.filter((item) => {
+    //   // return contains(name, formattedQuery);
+    //   return item.name.toLowerCase().includes(formattedQuery);
+    // });
+    // setFilteredHeroNames(data);
+    setQuery(text);
+    try {
+      setLoading(true);
+      const request = await axios.get(
+        `games?search=${text}&key=a5dc51990cf541aba0f759e85e41a324&ordering=~rating`
+      );
+
+      setGames(request.data.results);
+      setLoading(false);
+      // console.log(games);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
   const renderItem = ({ item, index }) => (
     <GameCard key={index} game={item} navigation={navigation} />
+  );
+
+  const renderGameInfoItem = ({ item, index }) => (
+    <GameInfoCard key={index} game={item} navigation={navigation} />
+  );
+
+  const renderWideItem = ({ item, index }) => (
+    <GameCardWide key={index} game={item} navigation={navigation} />
   );
 
   const renderChip = ({ item, index }) => (
@@ -169,7 +251,7 @@ const HomeScreen = ({ navigation }) => {
       key={index}
       type={selectedChip === item.id ? "solid" : "outline"}
       title={item.title}
-      onPress={() => fetchGames(item.search, item.id)}
+      onPress={() => fetchGames(item)}
       buttonStyle={{
         // backgroundColor: COLORS.mediumGrey,
         borderWidth: 2,
@@ -208,12 +290,16 @@ const HomeScreen = ({ navigation }) => {
   );
 
   useEffect(() => {
-    fetchGames(fetchTrendingGames, 0);
+    fetchGames(chips[selectedChip]);
+    setQuery("");
+    Keyboard.dismiss;
   }, []);
 
-  // useEffect(() => {
-  //   setRefresh(!refresh);
-  // }, [games]);
+  useEffect(() => {
+    if (query == "") {
+      fetchGames(chips[selectedChip]);
+    }
+  }, [query]);
 
   return (
     <ImageBackground
@@ -225,8 +311,35 @@ const HomeScreen = ({ navigation }) => {
         style={styles.appContainer}
         edges={["right", "top", "left"]}
       >
+        <StatusBar
+          // translucent
+          backgroundColor={COLORS.darkGrey}
+          barStyle="light-content"
+          style={{ zIndex: 30 }}
+        />
         <View style={styles.header}>
-          <Text style={{ ...styles.h2, paddingHorizontal: 15 }}>ARCADE</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 5,
+            }}
+          >
+            <Text style={{ ...styles.h2, paddingHorizontal: 15 }}>ARCADE</Text>
+
+            <SearchBar
+              placeholder="Search..."
+              onChangeText={handleSearch}
+              value={query}
+              containerStyle={styles.inputContainer}
+              inputContainerStyle={styles.input}
+              // onClear={fetchGames(chips[selectedChip])}
+              leftIconContainerStyle={{ left: 5 }}
+              inputStyle={{ ...styles.p, fontSize: 16 }}
+              searchIcon={{ size: 25 }}
+              round={true}
+            />
+          </View>
 
           <FlatList
             data={chips}
@@ -239,7 +352,8 @@ const HomeScreen = ({ navigation }) => {
               // width: "100%",
               alignItems: "center",
               justifyContent: "space-evenly",
-              // marginBottom: 30,
+              // marginTop: 10,
+              marginHorizontal: 5,
               height: 50,
             }}
           />
@@ -273,16 +387,42 @@ const HomeScreen = ({ navigation }) => {
                   left: 2,
                 }}
               >
-                Loading {chips[selectedChip].title} Games...
+                Loading {gameSectionTitle} Games...
               </Text>
             </View>
           </View>
         ) : (
-          <>
-            <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
+          <Animated.View
+            style={{
+              transform: query == "" ? [{ translateY: translateY }] : null,
+            }}
+          >
+            {query == "" ? (
+              <Animated.View style={{ opacity: fadeOutOnScroll }}>
+                <Carousel
+                  data={games.slice(0, 4)}
+                  sliderWidth={Dimensions.get("window").width}
+                  itemWidth={300}
+                  renderItem={renderWideItem}
+                  loop={true}
+                  scrollEnabled={!gameLoading}
+                  inactiveSlideShift={0}
+                  inactiveSlideOpacity={Platform.OS === "ios" ? 0.5 : 1}
+                />
+              </Animated.View>
+            ) : null}
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                marginBottom: 15,
+              }}
+            >
               <Icon
-                name={chips[selectedChip].iconName}
-                type={chips[selectedChip].iconType}
+                name={gameSectionIconName}
+                type={gameSectionIconType}
                 color="#517fa4"
               />
               <Text
@@ -290,31 +430,34 @@ const HomeScreen = ({ navigation }) => {
                   ...styles.h3,
                   textAlign: "right",
                   paddingHorizontal: 15,
-                  marginBottom: 10,
                 }}
               >
-                {chips[selectedChip].title} Games
+                {gameSectionTitle} Games
               </Text>
             </View>
-            <FlatList
-              key={1}
-              data={games}
-              extraData={games}
-              numColumns={2}
-              renderItem={renderItem}
-              keyExtractor={(item) => item.id.toString()}
-              contentContainerStyle={{ alignItems: "center" }}
-            />
-          </>
+            <View>
+              <FlatList
+                data={games.slice(5, games.length)}
+                extraData={games}
+                onScroll={(e) => {
+                  scrollY.setValue(e.nativeEvent.contentOffset.y);
+                  Keyboard.dismiss;
+                }}
+                keyboardShouldPersistTaps={"always"}
+                keyboardDismissMode={"on-drag"}
+                scrollEventThrottle={16}
+                renderItem={renderGameInfoItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                  flexGrow: 1,
+                  paddingBottom: 450,
+                  top: query == "" ? 0 : 0,
+                  alignItems: "center",
+                }}
+              />
+            </View>
+          </Animated.View>
         )}
-
-        {/* <FlatList
-          data={games}
-          extraData={games}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ alignItems: "center" }}
-        /> */}
       </SafeAreaView>
     </ImageBackground>
   );
@@ -328,7 +471,7 @@ const styles = StyleSheet.create({
   },
   appContainer: {
     flex: 1,
-    backgroundColor: "transparent",
+    backgroundColor: COLORS.darkGrey,
     // paddingBottom: -40,
     // justifyContent: "center",
     // alignItems: "center",
@@ -348,6 +491,30 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: COLORS.lightGrey,
   },
+  inputContainer: {
+    // width: "95%",
+    width: 200,
+    height: 40,
+    // paddingHorizontal: 10,
+    // marginBottom: 8,
+    backgroundColor: COLORS.navy,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "transparent",
+    borderTopColor: "transparent",
+    // borderWidth: 2,
+    borderRadius: 30,
+    borderWidth: 0,
+    zIndex: 2,
+  },
+  input: {
+    // backgroundColor: "transparent",
+    width: "100%",
+    height: 40,
+    borderColor: COLORS.navy,
+    // borderWidth: 2,
+    borderRadius: 20,
+  },
   grainOverlay: {
     flex: 1,
     width: "100%",
@@ -356,7 +523,9 @@ const styles = StyleSheet.create({
   },
   header: {
     // paddingHorizontal: 15,
-    marginBottom: 10,
+    zIndex: 1000,
+    backgroundColor: COLORS.darkGrey,
+    paddingBottom: 15,
   },
 });
 
